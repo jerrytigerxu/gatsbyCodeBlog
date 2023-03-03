50869dc6c9b628cb53ef2447a74541e4345a7a2c
jerrytigerxu <jere.tiger.xu@gmailc.om>
2023-03-03 17:25:53 -0500

    Finished author pages

---
+++ src/pages/posts/004-fourth-post.md
@@ -0,0 +1,23 @@
+---
+title: "Fourth Post - CSS"
+date: 2018-12-03 07-00:00
+author: "John Doe"
+image: ../../images/code.jpg
+tags:
+- design
+- tutorial
+- Crazy Crazy Web Design
+---
+
+This time we will talk about CSS and you're going to love it or else!
+This time we will talk about CSS and you're going to love it or else!
+This time we will talk about CSS and you're going to love it or else!
+This time we will talk about CSS and you're going to love it or else!
+This time we will talk about CSS and you're going to love it or else!
+This time we will talk about CSS and you're going to love it or else!
+This time we will talk about CSS and you're going to love it or else!
+This time we will talk about CSS and you're going to love it or else!
+This time we will talk about CSS and you're going to love it or else!
+This time we will talk about CSS and you're going to love it or else!
+This time we will talk about CSS and you're going to love it or else!
+This time we will talk about CSS and you're going to love it or else!
--- src/pages/team.js
+++ src/pages/team.js
@@ -3,11 +3,46 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
+import authors from '../util/authors'
+import { Row, Card, CardText, CardBody, CardTitle, Button } from 'reactstrap'
+import JohnImage from '../images/man.jpg'
+import JaneImage from '../images/woman.jpg'
+import { slugify } from '../util/utilityFunctions'
 
 const TeamPage = () => (
   <Layout pageTitle="Our Team">
-   <Seo title="Team" keywords={['gatsby', 'application', 'react']} /> 
+   <Seo title="Our Team" keywords={['gatsby', 'application', 'react']} /> 
+   <Row className="mb-4">
+    <div className="col-md-3">
+      <img src={JohnImage} style={{ maxWidth: '100%'}} alt="John profile" />
+    </div>
+
+    <div className="col-md-8">
+      <Card style={{ minHeight: '100%'}}>
+        <CardBody>
+          <CardTitle>{authors[0].name}</CardTitle>
+          <CardText>{authors[0].bio}</CardText>
+          <Button color="primary" href={`/author/${slugify(authors[0].name)}`}>View posts</Button>
+        </CardBody>
+      </Card>
+    </div>
+   </Row>
+
+   <Row className="mb-4">
+    <div className="col-md-3">
+      <img src={JaneImage} style={{ maxWidth: '100%'}} alt="Jane profile" />
+    </div>
+
+    <div className="col-md-8">
+      <Card style={{ minHeight: '100%'}}>
+        <CardBody>
+          <CardTitle>{authors[1].name}</CardTitle>
+          <CardText>{authors[1].bio}</CardText>
+          <Button color="primary" href={`/author/${slugify(authors[1].name)}`}>View posts</Button>
+        </CardBody>
+      </Card>
+    </div>
+   </Row>
   </Layout>
 )
 
